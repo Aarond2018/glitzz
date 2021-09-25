3c0c310a62b8c5b304b8d2c57e1aa34c84add931
Aarond2018 <aarondamilola1998@gmail.com>
2021-09-25 14:15:28 -0700

    added the header-top

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,9 +4,20 @@ import styles from './header.module.css'
 
 export default function Header() {
   return (
-    <div>
-      yeeee
-    </div>
+    <header>
+      <div className={styles["header-top"]}>
+        <div className={`${styles.container} container`}>
+          <p>Welcome to Glitzz store.....</p>
+          <ul>
+            <li><a href=""><i className="fas fa-map-marker-alt"></i>Contact</a></li>
+            <li><a href=""><i className="far fa-question-circle"></i>Need help</a></li>
+            <li><a href=""><i className="far fa-user"></i>Sign in</a><a href=""> / Register</a></li>
+          </ul>
+        </div>
+      </div>
+    </header>
   )
 }
 
+
+{/* <i class="fas fa-store-alt"></i> */}
